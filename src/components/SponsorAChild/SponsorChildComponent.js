d8cf72f9950b7065d09ae7c4fcc6ec878cdf1c56
leianne louis <leilouis112@gmail.com>
2019-03-21 15:21:18 -0400

    added mock data for sponser child view

--- src/components/SponsorAChild/SponsorChildComponent.js
+++ src/components/SponsorAChild/SponsorChildComponent.js
@@ -1,44 +1,49 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import "./SponsorAChild.css";
 
 function SponsorChildComponent(props) {
   return (
     <>
       <h1>Select a child to sponsor!</h1>
-      <Card >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-            </Typography>
-            <Typography component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+      <div className='studentCardContainer'>
+        {props.students.map(student => (
+          <Card className="studentCard">
+            <CardActionArea>
+            {/* ADD ONCLICK TO CARD AREA ACTION THAT WILL LOAD SINGLE CHILD VIEW */}
+              <CardMedia 
+                className=''
+                component="img"
+                alt="Contemplative Reptile"
+                height="140"
+                background-size='cover'
+                src={`${student.img}`}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {`${student.student_fname} ${student.student_lname} `}
+                </Typography>
+                <Typography component="p" />
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Sponsor
+              </Button>
+              <Typography gutterBottom color="secondary" component="p">
+                    <p className='totalDueLabel'> Total Due: </p>{`${student.dues}`}
+                </Typography>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
