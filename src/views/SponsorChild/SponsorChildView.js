bae893c50b9a7898534bb2c6c0f9c9f28103a943
leianne louis <leilouis112@gmail.com>
2019-03-25 17:55:53 -0400

    refactored layout and added loader to view

---
+++ src/views/SponsorChild/SponsorChildView.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import axios from "axios";
+import SponsorChildComponent  from "../../components/SponsorAChild/SponsorChildComponent";
+import { ClipLoader } from "react-spinners";
+import './Sponsor.css'
+class SponsorChildView extends Component {
+  state = {
+    students: [],
+    selectedStudent: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://18.220.56.197:9000/api/students")
+      .then(res => {
+        this.setState({
+          students: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  sponsorSelected = (e, student) => {
+    e.preventDefault();
+    this.setState({ selectedStudent: student });
+  };
+
+  render() {
+    console.log(this.state);
+    if (this.state.students.length > 0) {
+      return (
+        <>
+          <SponsorChildComponent
+            sponsorSelected={this.sponsorSelected}
+            selectedStudent={this.state.selectedStudent}
+            students={this.state.students}
+          />
+        </>
+      );
+    } else {
+      return (
+        <div className='loaderContainer'>
+          <ClipLoader
+            css=""
+            sizeUnit={"px"}
+            size={150}
+            color={"#123abc"}
+            loading={this.state.students.length > 0 ? false : true}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+export default SponsorChildView;
--- src/views/index.js
+++ src/views/index.js
@@ -3,4 +3,4 @@
 export { default as LoginView } from './LoginView';
 export { default as HeaderView } from './HeaderView';
 export { default as BoardDashboard } from './BoardDashboard';
-export { default as SponsorChildView } from './SponsorChildView';
+export { default as SponsorChildView } from './SponsorChild/SponsorChildView';
