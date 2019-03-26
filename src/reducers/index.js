1e36b98ace044ecd6dab921a84dd07f669d3df14
spinon187 <spinon187@gmail.com>
2019-03-26 18:46:08 -0500

    improved redux flow(optional) and added a school/grade selector component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 //import reducers here as you write them
 import { StudentReducer } from './StudentReducer';
+import { SchoolReducer } from './SchoolReducer';
+import { VisitReducer } from './VisitReducer';
 
 const rootReducer = combineReducers({
   //combine reducers in here
   //make sure the name here matches your view
   students: StudentReducer,
-  
+  schools: SchoolReducer,
+  visits: VisitReducer,
 });
 
 export default rootReducer;
