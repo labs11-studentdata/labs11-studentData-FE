470bd9ae1422a0aadf6cdd8f59cfb72f975a52d8
spinon187 <spinon187@gmail.com>
2019-03-27 17:13:13 -0500

    merging

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,6 @@ import { StudentReducer } from './StudentReducer';
 import { SchoolReducer } from './SchoolReducer';
 import { VisitReducer } from './VisitReducer';
 import { LoginReducer } from './LoginReducer';
-<<<<<<< HEAD
-=======
-
->>>>>>> d126c3ef29feae26fb372b6198f355300329dd47
 
 const rootReducer = combineReducers({
   //combine reducers in here
