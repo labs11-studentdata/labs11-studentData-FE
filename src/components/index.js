638f3b80bc7423a5da174d75657de2b83d7fd537
Jasper <joshtietz11@gmail.com>
2019-03-28 13:09:26 -0500

    add visits lists for the social worker components

--- src/components/index.js
+++ src/components/index.js
@@ -11,13 +11,10 @@ export { default as AddVisit } from './SocialWorker/addVisit.js'
 export { default as UpdateVisit } from './SocialWorker/updateVisit.js'
 export { default as DeleteVisitButton } from './SocialWorker/deleteVisitButton.js'
 export { default as SingleVisit } from './SocialWorker/singleVisit.js'
-<<<<<<< Updated upstream
 export { default as SchoolSelect } from './SchoolSelect';
 export { default as EditStudent } from './StudentView/EditStudent';
 export { default as AddStudent } from './StudentView/AddStudent';
 //export { default as DeleteStudent } from './StudentView/DeleteStudent';
 export { default as SmallSponsor } from './BoardMember/SmallSponsor';
-=======
 export { default as AllVisits } from './SocialWorker/VisitLists/allVisits.js'
 export { default as VisitsBySchool } from './SocialWorker/VisitLists/visitsBySchool.js'
->>>>>>> Stashed changes
