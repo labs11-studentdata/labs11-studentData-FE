9f78eb39a4120175e8e9324be05157495cf15d74
spinon187 <spinon187@gmail.com>
2019-03-28 20:12:47 -0500

    merging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import requiresAuth from './auth/requiresAuth';
 import { Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { LoginView, HeaderView, SponsorChildView, BoardView, StudentView, SocialWorkerView } from './views';
-=======
-import {  HeaderView, SponsorChildView, BoardView, StudentView, AdminDashboard } from './views';
+import { HeaderView, SponsorChildView, BoardView, StudentView, SocialWorkerView, AdminDashboard  } from './views';
 import LoginView from './login/LoginView';
->>>>>>> master
 import { Onboarding, AddStudent } from './components'
 
 
@@ -20,7 +16,7 @@ class App extends Component {
         <Route path='/onboarding' component={Onboarding} />
         <Route path='/sponsor' component={SponsorChildView} />
         <Route path='/board' component={BoardView} />
-        {/* <Route path='/login' component={LoginView} /> */}
+        <Route path='/login' component={LoginView} />
         <Route path='/student/:id' render={props => <StudentView {...props} />} />
         <Route path='/add' component={AddStudent} />
         <Route path='/social' component={SocialWorkerView} />        
