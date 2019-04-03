dc01c4d5245725661669af8af9505376896f152b
Kevin <brackkevin@gmail.com>
2019-04-03 16:31:13 -0500

    Built theme object

---
+++ src/styles/MUITheme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import red from '@material-ui/core/colors/red';
+import grey from '@material-ui/core/colors/grey';
+
+// To see what is available to override in the theme object
+// https://material-ui.com/customization/default-theme/
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blueGrey,
+        secondary: grey,
+        error: red,
+        background: {
+            paper: "#eee",
+            default: "#ddd",
+        }
+      },
+});
+
+export default theme;
