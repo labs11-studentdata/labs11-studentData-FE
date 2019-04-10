364f3c4a6613871e94c463d214520f5aea22b1a9
Kevin <brackkevin@gmail.com>
2019-04-10 17:29:33 -0500

    Board Social table built and working

--- src/components/BoardMember/BoardSocial.js
+++ src/components/BoardMember/BoardSocial.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+  tableRoot: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
 const BoardSocial = props => {
-  return (
-    <div>
-      BOARD SOCIAL VISITS TABLE
-    </div>
-  )
+  const {classes} = props;
+  console.log("BOARD SOCIAL PROPS", props);
+  if(!props.schoolID){
+    return(
+      <Fragment>
+        <Typography variant="subtitle1">
+          Please select a school to view social worker notes.
+        </Typography>
+      </Fragment>
+    )
+  } else if (props.socialVisits.length === 0) {
+    return(
+      <Fragment>
+        <Typography variant="subtitle1">
+          There are no social worker visits on record for the selected school.
+        </Typography>
+      </Fragment>
+    )
+  } else {
+    const socialVisits = props.socialVisits;
+    console.log("SOCIAL VISITS", socialVisits);
+    return(
+      <Fragment>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Note</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {socialVisits.map(visit => (
+              <TableRow key={visit.visitID}>
+                <TableCell>{visit.visit_date}</TableCell>
+                <TableCell>{visit.notes}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Fragment>
+    )
+  }
 }
 
-export default BoardSocial;
+export default withStyles(styles)(BoardSocial);
