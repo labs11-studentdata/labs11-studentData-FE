0f4e5688901d126873862552ab840e72733683f4
Leianne Louis <leiannelouis@Leiannes-MacBook-Pro.local>
2019-04-11 19:17:40 -0400

    updated landing page btn

--- src/components/LandingPage/LandingCardBtn.js
+++ src/components/LandingPage/LandingCardBtn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const LandingCardBtn = props => {
   
@@ -12,9 +13,14 @@ const LandingCardBtn = props => {
             </p>
             {/* <div class="button" onClick={props.btnHandler}>{props.btnText}</div> */}
             {/* Switched to href for presentation */}
-            <a href={`${process.env.REACT_APP_FE_ROOT}/login`}>
-            <div class="button" >{props.btnText}</div>
-            </a>
+            <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            href={`${process.env.REACT_APP_FE_ROOT}/login`}
+>
+            Register Now
+          </Button>
         </div>
     );
   }
