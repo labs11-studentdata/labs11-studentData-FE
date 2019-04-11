e7a3d0129e8207f7f0fd50f704be04c605f4688a
Kevin <brackkevin@gmail.com>
2019-04-11 15:22:19 -0500

    Removed extra scrollbar for all dashboards

--- src/views/DashboardFrame.js
+++ src/views/DashboardFrame.js
@@ -49,7 +49,7 @@ function DashContainer(props) {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={16} style={{ overflowY: "scroll", width: "100%" }}>
+      <Grid container spacing={16} style={{  width: "100%" }}>
         {/* LEFT NAV */}
         <Grid item xs={12} md={3}>
           <Paper className={classes.sidenav}>
