f776dcb7c00f3126108b2a85d5b7cc65ca1f5745
Leianne Louis <leiannelouis@Leiannes-MacBook-Pro.local>
2019-04-16 17:45:29 -0400

    adds my account view and dashboard link

--- src/views/HeaderView.js
+++ src/views/HeaderView.js
@@ -4,7 +4,8 @@ import { HeaderComponent } from "../components";
 import { logoutUser, regSelected, logSelected } from "../actions/login";
 import { connect } from "react-redux";
 import axios from 'axios';
-// import HeaderStyles from '../styles/HeaderStyles.css'
+import HeaderStyles from '../styles/HeaderStyles.css'
+
 class HeaderView extends Component {
   state = {
     user: {}
@@ -33,6 +34,9 @@ class HeaderView extends Component {
       this.props.logSelected();
     }
   };
+
+ 
+
   render() {
     // console.log(this.state.user.photo_url)
     return (
@@ -42,16 +46,19 @@ class HeaderView extends Component {
         isRegistering={this.props.isRegistering}
         loggedIn={this.props.loggedIn}
         handleLogout={this.props.logoutUser}
+        accountType={this.props.accountType}
       />
     );
   }
 }
 
 const mapStateToProps = state => {
+  console.log(state)
   return {
     loggedIn: state.login.loggedIn,
     isRegistering: state.login.isRegistering,
-    userID: state.login.user.user_id
+    userID: state.login.user.user_id,
+    accountType: state.login.user.account_type
   };
 };
 export default connect(
