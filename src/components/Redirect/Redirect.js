2d3cbc1e69f097b1f5fed2b6fc57451add70e8e5
Kevin Brack <brackkevin@gmail.com>
2019-04-17 12:19:07 -0500

    Plugged in withStyles, added top margin

--- src/components/Redirect/Redirect.js
+++ src/components/Redirect/Redirect.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const Redirect = () => {
+const styles = {
+  message: {
+    marginTop: 200,
+  },
+};
+
+const Redirect = props => {
+  const { classes } = props;
   return (
-    <Typography variant="h4">
+    <Typography variant="h4" className={ classes.message }>
       REDIRECTING PLEASE WAIT...
     </Typography>
   )
 }
 
-export default Redirect;
+export default withStyles(styles)(Redirect);
