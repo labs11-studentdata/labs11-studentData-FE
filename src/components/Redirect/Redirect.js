33014c31f545fe564e731bce2b9953cf44a20f05
Kevin Brack <brackkevin@gmail.com>
2019-04-17 12:10:32 -0500

    Basic redirect component created

---
+++ src/components/Redirect/Redirect.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+const Redirect = () => {
+  return (
+    <Typography variant="h4">
+      REDIRECTING PLEASE WAIT...
+    </Typography>
+  )
+}
+
+export default Redirect;
