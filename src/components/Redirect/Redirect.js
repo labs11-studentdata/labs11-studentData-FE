39ed1f60a942c66b55966f342582e19970d2dc96
Kevin Brack <brackkevin@gmail.com>
2019-04-17 12:50:59 -0500

    Adds checks for user types and routes

--- src/components/Redirect/Redirect.js
+++ src/components/Redirect/Redirect.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom';
 
 const styles = {
   message: {
@@ -10,6 +11,19 @@ const styles = {
 
 const Redirect = props => {
   const { classes } = props;
+  const token = localStorage.getItem('jwt');
+  if(token === null){
+    props.history.push(`${process.env.REACT_APP_FE_ROOT}/home`);
+  } else if (token.includes("account_type=admin")){
+    props.history.push(`${process.env.REACT_APP_FE_ROOT}/admindashboard`);
+  } else if (token.includes("account_type=socialworker")) {
+    props.history.push(`${process.env.REACT_APP_FE_ROOT}/socialworkerdashboard`);
+  } else if (token.includes("account_type=boardmember")) {
+    props.history.push(`${process.env.REACT_APP_FE_ROOT}/boardmemberdashboard`);
+  } else {
+    props.history.push(`${process.env.REACT_APP_FE_ROOT}/onboarding`);
+  }
+
   return (
     <Typography variant="h4" className={ classes.message }>
       REDIRECTING PLEASE WAIT...
@@ -17,4 +31,4 @@ const Redirect = props => {
   )
 }
 
-export default withStyles(styles)(Redirect);
+export default withRouter(withStyles(styles)(Redirect));
